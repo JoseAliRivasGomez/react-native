557ee9df13f1c1feb31fdc9788c38c9ac8ba2e98
José Alí Rivas Gómez <100250771+JoseAliRivasGomez@users.noreply.github.com>
2023-09-11 15:54:21 -0600

    done

--- 10-ProductosApp/src/api/cafeApi.tsx
+++ 10-ProductosApp/src/api/cafeApi.tsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const baseURL = 'http://192.168.1.9:4000/api';
-// const baseURL = 'https://cafe-node-rn.up.railway.app/api';
+// const baseURL = 'http://192.168.1.9:4000/api';
+const baseURL = 'https://cafe-node-rn.up.railway.app/api';
 
 const cafeApi = axios.create({ baseURL });
 
